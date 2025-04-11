7125e571cd9b7d65096814463bd263caf49dacc8
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2025-04-11 10:15:44 +0200

    new normal integration interface

--- mbipy/normal_integration.py
+++ mbipy/normal_integration.py
@@ -1,10 +1,19 @@
 """Normal integration methods for phase integration from gradients."""
 
-__all__ = ("arnison", "dct_poisson", "dst_poisson", "frankot", "kottler")
+__all__ = (
+    "Li",
+    "Southwell",
+    "arnison",
+    "dct_poisson",
+    "dst_poisson",
+    "frankot",
+    "kottler",
+    "li",
+    "southwell",
+)
 
+from .src.config import __have_scipy__
 from .src.normal_integration import arnison, dct_poisson, dst_poisson, frankot, kottler
 
-
-# from .src.normal_integration.least_squares.li import li
-# from .src.normal_integration.least_squares.southwell import southwell
-
+if __have_scipy__:  # ??? do i need this? probably not
+    from .src.normal_integration import Li, Southwell, li, southwell
