0fe9f9c547a4bca8c5198ff78860fdb132c8a61a
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2025-05-09 21:59:20 +0200

    Refactor frankot function: update padding import, change pad parameter to "antisymmetric", and improve dtype handling

--- mbipy/src/normal_integration/fourier/frankot.py
+++ mbipy/src/normal_integration/fourier/frankot.py
@@ -12,7 +12,7 @@ __all__ = ("frankot",)
 
 from typing import TYPE_CHECKING, Literal
 
-from mbipy.src.normal_integration.fourier.padding import antisym
+from mbipy.src.normal_integration.fourier.padding import antisymmetric
 from mbipy.src.normal_integration.fourier.utils import (
     fft_2d,
     ifft_2d,
@@ -20,9 +20,16 @@ from mbipy.src.normal_integration.fourier.utils import (
     rfft_2d,
 )
 from mbipy.src.normal_integration.utils import check_shapes
-from mbipy.src.utils import array_namespace, cast_scalar, idiv, setitem
+from mbipy.src.utils import (
+    array_namespace,
+    astype,
+    cast_scalar,
+    get_dtypes,
+    idiv,
+    setitem,
+)
 
-if TYPE_CHECKING:
+if TYPE_CHECKING:  # pragma: no cover
     from numpy import floating
     from numpy.typing import NDArray
 
@@ -30,7 +37,7 @@ if TYPE_CHECKING:
 def frankot(
     gy: NDArray[floating],
     gx: NDArray[floating],
-    pad: Literal["antisym"] | None = None,
+    pad: Literal["antisymmetric"] | None = None,
     workers: int | None = None,
     use_rfft: bool | None = None,
 ) -> NDArray[floating]:
@@ -47,7 +54,7 @@ def frankot(
     gx : (..., M, N) NDArray[floating]
         Horizontal gradient(s).
     pad : Literal["antisym"] | None, optional
-        Type of padding to apply: "antisym" | None , by default None
+        Type of padding to apply: "antisymmetric" | None , by default None
     workers : int | None, optional
         Passed to scipy.fft fftn & ifftn, by default None
     use_rfft : bool | None, optional
@@ -67,48 +74,40 @@ def frankot(
 
     """
     xp = array_namespace(gy, gx)
-    dtype = xp.result_type(gy, gx)
-    if not xp.isdtype(dtype, "real floating"):
-        msg = "Input arrays must be real-valued."
-        raise ValueError(msg)
+    dtype, cdtype = get_dtypes(gy, gx)
     y, x = check_shapes(gx, gy)
     y2, x2 = 2 * y if pad else y, 2 * x if pad else x
 
-    if pad == "antisym":
-        gy, gx = antisym(gy=gy, gx=gx)
+    if pad == "antisymmetric":
+        gy, gx = antisymmetric(gy=gy, gx=gx)
     elif pad is None:
         pass
     else:
         msg = f"Invalid value for pad: {pad}"
         raise ValueError(msg)
 
-    if use_rfft:
-        fx = xp.astype(xp.fft.rfftfreq(x2), dtype, copy=False)
-    else:
-        fx = xp.astype(xp.fft.fftfreq(x2), dtype, copy=False)
-    fy = xp.astype(xp.fft.fftfreq(y2)[:, None], dtype, copy=False)
+    fx = astype(xp.fft.rfftfreq(x2) if use_rfft else xp.fft.fftfreq(x2), dtype)
+    fy = astype(xp.fft.fftfreq(y2)[:, None], dtype)
 
     if use_rfft:
         s = (y2, x2)
         gx_fft = rfft_2d(gx, s=s, workers=workers)
         gy_fft = rfft_2d(gy, s=s, workers=workers)
     else:
-        gx_fft = fft_2d(gx, workers=workers)
-        gy_fft = fft_2d(gy, workers=workers)
+        gx_fft = fft_2d(astype(gx, cdtype, copy=True), workers=workers)
+        gy_fft = fft_2d(astype(gy, cdtype, copy=True), workers=workers)
 
     # !!! Cast scalars to the same dtype as the result. Necessary for Numba.
-    two_j = cast_scalar(2j, xp.result_type(gx_fft, gy_fft))
+    two_j = cast_scalar(2j, cdtype)
     pi = cast_scalar(xp.pi, dtype)
-    zero = cast_scalar(0.0, dtype)
-    one = cast_scalar(1.0, dtype)
 
-    # TODO(nin17): inplace
+    # ??? do inplace instead
     f_num = fx * gx_fft + fy * gy_fft
     f_den = two_j * pi * (fx * fx + fy * fy)
 
-    f_den = setitem(f_den, (..., 0, 0), one)  # avoid division by zero warning
+    f_den = setitem(f_den, (..., 0, 0), 1.0)  # avoid division by zero warning
     frac = idiv(f_num, (...,), f_den)  # f_num is frac
-    frac = setitem(frac, (..., 0, 0), zero)
+    frac = setitem(frac, (..., 0, 0), 0.0)
     if use_rfft:
         return irfft_2d(frac, s=s, workers=workers)[..., :y, :x]
     return xp.real(ifft_2d(frac, workers=workers)[..., :y, :x])
