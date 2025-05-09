1ca5873530a46aa04016f2ffb80fea96a261a2fe
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2025-05-09 21:57:22 +0200

    Add future annotations import and reorder __all__ exports in Fourier module

--- mbipy/src/normal_integration/fourier/__init__.py
+++ mbipy/src/normal_integration/fourier/__init__.py
@@ -1,7 +1,8 @@
 """Normal integration using Fourier methods."""
 
-__all__ = ("arnison", "dct_poisson", "dst_poisson", "frankot", "kottler")
+from __future__ import annotations
 
+__all__ = ("arnison", "dct_poisson", "dst_poisson", "frankot", "kottler")
 
 from .arnison import arnison
 from .dct import dct_poisson
