c121667502cd1c659044ca815c1c20481c52787e
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2025-05-09 21:57:14 +0200

    Add normal integration module and padding functions

---
+++ mbipy/normal_integration/__init__.py
@@ -0,0 +1,27 @@
+"""Normal integration methods for phase integration from gradients."""
+
+from __future__ import annotations
+
+__all__ = (
+    "Li",
+    "Southwell",
+    "arnison",
+    "dct_poisson",
+    "dst_poisson",
+    "frankot",
+    "kottler",
+    "li",
+    "southwell",
+)
+
+from mbipy.src.normal_integration import (
+    Li,
+    Southwell,
+    arnison,
+    dct_poisson,
+    dst_poisson,
+    frankot,
+    kottler,
+    li,
+    southwell,
+)
---
+++ mbipy/normal_integration/padding.py
@@ -0,0 +1,7 @@
+"""Padding functions for normal integration."""
+
+from __future__ import annotations
+
+__all__ = ("antisymmetric",)
+
+from mbipy.src.normal_integration.fourier.padding import antisymmetric
