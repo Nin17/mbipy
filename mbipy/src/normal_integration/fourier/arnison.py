8de2c661e93a2624ca2d3711520dabffb8310a1a
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2025-05-09 21:57:34 +0200

    Refactor padding import and update parameter documentation in arnison function

--- mbipy/src/normal_integration/fourier/arnison.py
+++ mbipy/src/normal_integration/fourier/arnison.py
@@ -11,7 +11,7 @@ __all__ = ("arnison",)
 
 from typing import TYPE_CHECKING, Literal
 
-from mbipy.src.normal_integration.fourier.padding import antisym
+from mbipy.src.normal_integration.fourier.padding import antisymmetric
 from mbipy.src.normal_integration.fourier.utils import (
     fft_2d,
     ifft_2d,
@@ -19,9 +19,17 @@ from mbipy.src.normal_integration.fourier.utils import (
     rfft_2d,
 )
 from mbipy.src.normal_integration.utils import check_shapes
-from mbipy.src.utils import array_namespace, cast_scalar, idiv, imul, setitem
+from mbipy.src.utils import (
+    array_namespace,
+    astype,
+    cast_scalar,
+    get_dtypes,
+    idiv,
+    imul,
+    setitem,
+)
 
-if TYPE_CHECKING:
+if TYPE_CHECKING:  # pragma: no cover
     from numpy import floating
     from numpy.typing import NDArray
 
@@ -45,8 +53,8 @@ def arnison(
         Vertical gradient(s).
     gx : (..., M, N) NDArray[floating]
         Horizontal gradient(s).
-    pad : Literal["antisym"] | None, optional
-        Type of padding to apply: "antisym" | None, by default None
+    pad : Literal["antisymmetric"] | None, optional
+        Type of padding to apply: "antisymmetric" | None, by default None
     workers : int | None, optional
         Passed to scipy.fft fft2 & ifft2, by default None
     use_rfft : bool | None, optional
@@ -66,53 +74,44 @@ def arnison(
         If the pad argument is not None or "antisym".
     """
     xp = array_namespace(gy, gx)
-    dtype = xp.result_type(gy, gx)
-    cdtype = xp.result_type(dtype, xp.complex64)
-    if not xp.isdtype(dtype, "real floating"):
-        msg = "Input arrays must be real-valued."
-        raise ValueError(msg)
+    dtype, cdtype = get_dtypes(gy, gx)
     y, x = check_shapes(gx, gy)
     y2, x2 = 2 * y if pad else y, 2 * x if pad else x
 
-    if pad == "antisym":
-        gy, gx = antisym(gy=gy, gx=gx)
+    if pad == "antisymmetric":
+        gy, gx = antisymmetric(gy=gy, gx=gx)
     elif pad is None:
         pass
     else:
         msg = f"Invalid value for pad: {pad}"
         raise ValueError(msg)
 
-    if use_rfft:
-        fx = xp.astype(xp.fft.rfftfreq(x2), dtype, copy=False)
-    else:
-        fx = xp.astype(xp.fft.fftfreq(x2), dtype, copy=False)
-    fy = xp.astype(xp.fft.fftfreq(y2), dtype, copy=False)
+    fx = astype(xp.fft.rfftfreq(x2) if use_rfft else xp.fft.fftfreq(x2), dtype)
+    fy = astype(xp.fft.fftfreq(y2), dtype)
 
     # !!! Cast scalars to the same dtype as the result. Necessary for Numba.
-    zero = cast_scalar(0.0, dtype)
-    one = cast_scalar(1.0, dtype)
-    pi = cast_scalar(xp.pi, dtype)
     one_j = cast_scalar(1j, cdtype)
     two_j = cast_scalar(2j, cdtype)
 
-    fx = imul(fx, slice(0, None), pi)
-    fy = imul(fy, slice(0, None), pi)
+    fx = imul(fx, slice(0, None), xp.pi)
+    fy = imul(fy, slice(0, None), xp.pi)
 
-    sinfx = xp.sin(fx)  # TODO(nin17): inplace
-    sinfy = xp.sin(fy)
+    # ??? do inplace instead
+    sinfx = xp.sin(fx)
+    sinfy = xp.sin(fy)[:, None]
 
     if use_rfft:
         s = (y2, x2)
         gxfft2 = rfft_2d(gx, s=s, workers=workers)
         gyfft2 = rfft_2d(gy, s=s, workers=workers)
-        f_num = gxfft2 + one_j * gyfft2
+        f_num = gxfft2 + one_j * gyfft2  # ??? do inplace instead
     else:
-        f_num = fft_2d(gx + one_j * gy, workers=workers)
-    f_den = two_j * (sinfx + one_j * sinfy[:, None])
+        f_num = fft_2d(gx + one_j * gy, workers=workers)  # ??? do inplace instead
+    f_den = two_j * (sinfx + one_j * sinfy)
 
-    f_den = setitem(f_den, (..., 0, 0), one)  # avoid division by zero warning
+    f_den = setitem(f_den, (..., 0, 0), 1.0)  # avoid division by zero warning
     frac = idiv(f_num, (...,), f_den)
-    frac = setitem(frac, (..., 0, 0), zero)
+    frac = setitem(frac, (..., 0, 0), 0.0)
     if use_rfft:
         return irfft_2d(frac, s=s, workers=workers)[..., :y, :x]
     return xp.real(ifft_2d(frac, workers=workers)[..., :y, :x])
