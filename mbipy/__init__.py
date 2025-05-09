01aea84922321e59809e15301caf64441ff3db36
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2025-05-09 23:33:02 +0200

    Refactor __init__.py: remove unused _init function and streamline imports

--- mbipy/__init__.py
+++ mbipy/__init__.py
@@ -1,16 +1,5 @@
 """Modulation based x-ray phase contrast imaging."""
 
+__all__ = ("config",)
 
-def _init() -> None:
-    """Import numba overloads."""
-    from mbipy.src.normal_integration.fourier.overloads import (
-        dct2_2d_overload,
-        dst1_2d_overload,
-        fft_2d_overload,
-        flip_overload,
-        idct2_2d_overload,
-        idst1_2d_overload,
-        ifft_2d_overload,
-        irfft_2d_overload,
-        rfft_2d_overload,
-    )
+from mbipy.src.config import config
