e8cf73fe3cec6c63c069dfd1f7f4490efec6657b
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2025-10-16 12:21:03 +0200

    planned overloads

---
+++ mbipy/src/normal_integration/least_squares/overloads.py
@@ -0,0 +1,5 @@
+"""Numba overloads for normal integration using sparse least squares methods."""
+
+from __future__ import annotations
+
+# TODO(nin17): numba overload of superlu, then li/Li & southwell/Southwell
