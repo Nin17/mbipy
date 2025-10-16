5ff243569e942f0394865dfd4a9d1c6b9e47ddb7
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2025-10-16 11:22:49 +0200

    move padding

--- mbipy/normal_integration/padding/__init__.py
+++ mbipy/normal_integration/padding/__init__.py
@@ -1,7 +1,5 @@
 """Padding functions for normal integration."""
 
-from __future__ import annotations
-
 __all__ = ("antisymmetric",)
 
 from mbipy.src.normal_integration.fourier.padding import antisymmetric
