8f8a39a7a31671bb4e5c2188f3db0dc995962d10
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2026-04-28 20:59:33 +0200

    tuple -> list __all__

--- mbipy/src/normal_integration/fourier/__init__.py
+++ mbipy/src/normal_integration/fourier/__init__.py
@@ -1,6 +1,6 @@
 """Normal integration using Fourier methods."""
 
-__all__ = ("arnison", "dct_poisson", "dst_poisson", "frankot", "kottler")
+__all__ = ["arnison", "dct_poisson", "dst_poisson", "frankot", "kottler"]
 
 from ._arnison import arnison
 from ._frankot import frankot
--- mbipy/src/normal_integration/least_squares/__init__.py
+++ mbipy/src/normal_integration/least_squares/__init__.py
@@ -1,6 +1,6 @@
 """Normal integration using sparse least squares methods."""
 
-__all__ = ("Li", "Southwell", "li", "southwell")
+__all__ = ["Li", "Southwell", "li", "southwell"]
 
 from ._li import Li, li
 from ._southwell import Southwell, southwell
