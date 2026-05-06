be66ba6970183bb4446d08c5bc8479fed0b12efa
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2026-05-06 17:34:38 +0200

    todo

--- mbipy/normal_integration/_utils.py
+++ mbipy/normal_integration/_utils.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from numpy import broadcast_shapes
+from numpy import broadcast_shapes  # TODO(nin17): remove - added to the standard
 
 if TYPE_CHECKING:
 
