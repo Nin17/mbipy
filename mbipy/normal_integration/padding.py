72194926933268de160b1bf45a976a843a30251f
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2026-05-06 17:39:35 +0200

    correct import

--- mbipy/normal_integration/padding.py
+++ mbipy/normal_integration/padding.py
@@ -8,4 +8,4 @@ from mbipy.normal_integration import padding
 
 __all__ = ["antisymmetric"]
 
-from mbipy.normal_integration.fourier._padding import antisymmetric
+from mbipy.normal_integration._fourier._padding import antisymmetric
